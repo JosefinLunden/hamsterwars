608a70e67ad29eed6212601dd0fe5807c2656f78
JosefinLunden <josefincslunden@hotmail.se>
2020-05-13 11:13:39 +0200

    added comments

--- routes/charts.js
+++ routes/charts.js
@@ -6,7 +6,9 @@ const { auth, db } = require ('./../firebase');
 router.get('/top', async (req,res) => {
 
   try {
+    // empty array to put the five hamsters in.
     let hamstersArray = []; 
+    // find the hamsters, limit(5) to only return five and orderby() sort the data returned in descending order. 
     let snapShot = await db.collection('hamsters').limit(5).orderBy('wins', 'desc').get()
 
     snapShot.forEach(doc => {
@@ -29,6 +31,7 @@ router.get('/bottom', async (req,res) => {
   try {
 
     let hamstersArray = []; 
+    
     let snapShot = await db.collection('hamsters').limit(5).orderBy('defeats', 'desc').get()
 
     snapShot.forEach(doc => {
